7c1ed83f372e3c8130ac72092e3f6eb6f8c005ce
tuanndph16237 <tuanndph16237@example.com>
2022-09-18 11:11:25 +0700

    exxercise 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule,
